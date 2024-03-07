1ee69507ad49d807c177de5f4b92a2bd2840a002
Aleksandra Badaczewska <dawid.aleksandra@gmail.com>
2024-03-07 16:58:28 -0600

    improved performance, removed temporary console logs

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,7 +1,7 @@
 
 // A function (called for each single.html based content) reformatting default markdown behavior for target links
 function adjustWrapperLinks() {
-  console.log("here");
+
   var targetLinks = document.querySelectorAll('p.wrapper');
   targetLinks.forEach(function(targetLink) {
     var aTag = targetLink.querySelector('a');
@@ -10,16 +10,20 @@ function adjustWrapperLinks() {
     if (next && next.tagName === 'P') {
       next.classList.add('wrapper-link');
     }
-    if (previous && previous.tagName === 'P' && aTag) {
-      previous.classList.add('wrapper-link');
-      previous.appendChild(aTag);
-    }
-    if (previous && previous.tagName === 'UL') {
-      var lastLi = previous.querySelector('li:last-child');
-      if (lastLi) {
-        lastLi.appendChild(aTag);
+
+    if (previous) {
+      if (previous.tagName === 'P' && aTag) {
+        previous.classList.add('wrapper-link');
+        previous.appendChild(aTag);
+      }
+      else if (previous.tagName === 'UL') {
+        var lastLi = previous.querySelector('li:last-child');
+        if (lastLi) {
+          lastLi.appendChild(aTag); // Append the targetLink to the last <li>
+        }
       }
     }
+
     if (!targetLink.innerHTML.trim()) {
       targetLink.parentNode.removeChild(targetLink);
     }
