b43488b59f6354cbdd6189feed8a3adf66349c61
Aleksandra Badaczewska <dawid.aleksandra@gmail.com>
2024-03-13 21:57:09 -0500

    reimplemented adjustWrapperLinks function to better match usage cases

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -4,17 +4,23 @@ function adjustWrapperLinks() {
 
   var targetLinks = document.querySelectorAll('p.wrapper');
   targetLinks.forEach(function(targetLink) {
-    var aTag = targetLink.querySelector('a');
+    var aTag = targetLink.querySelector('a');                                   //
+    if (targetLink.classList.contains('done')) {return;}
     var parent = targetLink.parentNode;
-    console.log(parent.tagName, aTag.text);
+    console.log(parent.tagName, aTag.text);                                     //
 
     var next = targetLink.nextElementSibling;
     var prev = targetLink.previousElementSibling;
-    if (next) {console.log("Next: ", next.tagName)}
-    if (prev) {console.log("Prev: ", prev.tagName)}
+    if (prev) {console.log("Prev: ", prev.tagName)}                             //
+    if (next) {console.log("Next: ", next.tagName)}                             //
     if (parent) {
       if (parent.tagName === 'SECTION') {
-        if (next && !(/^H[1-6]$/).test(next.tagName)) {next.classList.add('inline');}
+        var nextUlFound = true;
+        if (next && next.tagName === 'P') {
+          next.classList.add('inline');
+          if (/^[a-zA-Z]/.test(next.textContent)) {next.textContent = ' ' + next.textContent;}
+          nextUlFound = false;
+        }
         if (prev) {
           if (prev.tagName === 'P') {prev.classList.add('inline');}
 
@@ -22,17 +28,37 @@ function adjustWrapperLinks() {
             var lastLi = prev.querySelector('li:last-child');
             lastLi.appendChild(targetLink);
             var current = next;
-            var nextUlFound = false;
-            while (current && !nextUlFound) {
-              console.log("HERE: ", current.tagName, current);
+            while (current && current.tagName === 'P' && !nextUlFound) {
+              console.log("HERE: ", current.tagName, current);                  //
               if (current.textContent) {
                 if (/^[a-zA-Z]/.test(current.textContent)) {current.textContent = ' ' + current.textContent;}
+                var nodes = current.childNodes
+                if (nodes.length >= 2 && nodes[0].tagName === 'BR' && nodes[1].tagName === 'BR') {
+                  current.removeChild(current.firstElementChild);
+                  current.removeChild(current.firstElementChild);
+                  console.log("YES");                                           //
+                  break;
+                }
               }
+              if (current.classList.contains('wrapper')) {current.classList.add('done');}
+              else {current.classList.add('inline');}
+              var next2 = current.nextElementSibling;
               lastLi.appendChild(current);
-              var next2 = current.nextSibling;
               if (!next2 || next2.tagName === 'UL') {nextUlFound = true;}
               else {current = next2;}
             }
+
+            if (parent.tagName === 'SECTION' && targetLink.classList.contains('level-1')) {
+              console.log("FOUND");                                             //
+              var n = prev.previousElementSibling;
+              console.log(": ", n.tagName);                                     //
+              if (n && n.tagName === 'UL' && !prev.classList.contains('level-1')) {
+                n.appendChild(prev);
+                prev.classList.add('level-1');
+                console.log("prev: ", prev.tagName, prev.classList);            //
+              }
+            }
+
           }
 
         }
